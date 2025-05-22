f3eec13cbf91c393637f8f51fc12ce3bc51fc43e
Nicolas Belelli <nicolas.belelli@davinci.edu.ar>
2025-05-22 13:51:13 -0300

    New office WIP

--- Assets/Scripts/RotatingScript.cs
+++ Assets/Scripts/RotatingScript.cs
@@ -5,13 +5,7 @@ using UnityEngine;
 public class RotatingScript : MonoBehaviour
 {
     [SerializeField] float _rotatingSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
         transform.Rotate(Vector3.up * _rotatingSpeed * Time.deltaTime);
