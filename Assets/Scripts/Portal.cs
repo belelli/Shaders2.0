282adeac210e7776a9505ebeb8316a5bb519ecbf
Nicolas Belelli <nicolas.belelli@davinci.edu.ar>
2025-05-23 15:28:13 -0300

    portals

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    
+    public Transform target;
+    
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = target.position;
+        var player = other.GetComponent<PlayerMovement>();
+        if (player)
+        {
+            Debug.Log("coillider");
+            player.controller.enabled = false;
+            other.transform.position = target.position;
+            player.controller.enabled = true;
+
+        }
+        
+    }
+}
