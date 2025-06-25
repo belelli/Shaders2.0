a315ca0c730eabfbc1b59d93203ac0a22c2bad17
Nicolas Belelli <nicolas.belelli@davinci.edu.ar>
2025-06-25 00:10:28 -0300

    fixes on controler. door removed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -18,6 +18,11 @@ public class PlayerMovement : MonoBehaviour
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed *Time.deltaTime);
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Fix for CS1612: Modify the entire position vector instead of trying to modify a single component
+        Vector3 newPosition = transform.position;
+        newPosition.y = 0.9f;
+        transform.position = newPosition;
     }
 }
