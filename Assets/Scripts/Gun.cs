1caa45831c254e1d78ddafc8cab4871ccb1d5c64
Nicolas Belelli <179278269+belelli@users.noreply.github.com>
2026-07-11 11:59:53 -0300

    Hand Gun prefab + Spawn 2 types of bullets

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [Header("Bullet Prefabs")]
+    [SerializeField] private GameObject bulletType1;
+    [SerializeField] private GameObject bulletType2;
+
+    [Header("Spawn Point")]
+    [SerializeField] private Transform bulletOrigin;
+
+    private int currentBulletType = 1;
+
+    private void Update()
+    {
+        HandleWeaponSwitch();
+        HandleShoot();
+    }
+
+    private void HandleWeaponSwitch()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentBulletType = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentBulletType = 2;
+        }
+    }
+
+    private void HandleShoot()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bulletPrefab = null;
+
+        switch (currentBulletType)
+        {
+            case 1:
+                bulletPrefab = bulletType1;
+                break;
+
+            case 2:
+                bulletPrefab = bulletType2;
+                break;
+        }
+
+        if (bulletPrefab != null && bulletOrigin != null)
+        {
+            Instantiate(
+                bulletPrefab,
+                bulletOrigin.position,
+                bulletOrigin.rotation
+            );
+        }
+    }
+}
