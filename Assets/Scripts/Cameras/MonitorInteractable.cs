cb0ffdbbf76abb20d7d321b0ca9c6c79c53a00af
Nicolas Belelli <179278269+belelli@users.noreply.github.com>
2026-07-22 21:58:17 -0300

    Canvas to interact with Cameras

--- Assets/Scripts/Cameras/MonitorInteractable.cs
+++ Assets/Scripts/Cameras/MonitorInteractable.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,20 +6,35 @@ using UnityEngine;
 public class MonitorInteractable : MonoBehaviour
 {
     [SerializeField] private CameraMonitorManager cameraManager;
+    [SerializeField] private GameObject InteractCanvas;
     
 
     private bool playerInside;
 
+
+    private void Start()
+    {
+        InteractCanvas.SetActive(false);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
             playerInside = true;
+            InteractCanvas.SetActive(true);
+        }
+            
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
             playerInside = false;
+            InteractCanvas.SetActive(false);
+        }
+            
     }
 
     private void Update()
@@ -26,6 +42,7 @@ public class MonitorInteractable : MonoBehaviour
         if (playerInside && Input.GetKeyDown(KeyCode.E))
         {
             cameraManager.EnterMonitorMode();
+            InteractCanvas.SetActive(false);
         }
     }
 }
