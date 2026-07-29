8f5e6b861ab94aec234e15b6ce983f260243ea8c
Nicolas Belelli <179278269+belelli@users.noreply.github.com>
2026-07-29 14:52:57 -0300

    shield changes colors

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -5,22 +5,55 @@ using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
+    [SerializeField] private Renderer shieldRenderer;
+
     private int life = 3;
+
+    // Colores
+    [SerializeField] private Color life3Color; // Violeta
+    [SerializeField] private Color life2Color; // Naranja claro
+    [SerializeField] private Color life1Color; // Rojo
+
+    private void Start()
+    {
+        UpdateColor();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("normalBullet"))
         {
             Damage();
         }
-        
     }
 
     private void Damage()
     {
         life--;
+
+        UpdateColor();
+
         if (life <= 0)
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void UpdateColor()
+    {
+        switch (life)
+        {
+            case 3:
+                shieldRenderer.material.SetColor("_Color_Escudo", life3Color);
+                break;
+
+            case 2:
+                shieldRenderer.material.SetColor("_Color_Escudo", life2Color);
+                break;
+
+            case 1:
+                shieldRenderer.material.SetColor("_Color_Escudo", life1Color);
+                break;
         }
     }
 }
